042cee06da503076eb9bdf943f6c2a96431c3dbe
Charlie Nourrcier <cnourrcier@hotmail.com>
2024-06-06 18:07:15 -0500

    First commit. Prepare for deployment

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Navigate to client directory
+cd e-commerce-front-end-mern
+
+# Install frontend dependencies
+npm install
+
+# Build the client
+npm run build
+
+# Navigate to the server directory
+cd ..
+cd e-commerce-server-mern
+
+# Install dependencies
+npm install
